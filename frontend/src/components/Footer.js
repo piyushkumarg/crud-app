e11fdb4640ab8cd76106db48776003bb41aa6161
Piyush Kumar <piyushkumar1116@gmail.com>
2022-11-22 03:09:23 +0530

    new changes

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-dark text-center text-white mt-5">
+        <div className="container p-4 pb-0">
+          <section className="mb-4">
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i className="fab fa-google"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </section>
+        </div>
+
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2022 Copyright:
+          <span className="ms-2">Piyush Kumar</span>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
